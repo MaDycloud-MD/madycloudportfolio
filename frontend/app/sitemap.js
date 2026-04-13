7cd89494bb66b4884cf18c786cecbac745cd242d
MaDycloud-MD <md.shoaib.i.makandar@gmail.com>
2026-04-13 15:26:39 +0530

    Updated sitemaps

--- frontend/app/sitemap.js
+++ frontend/app/sitemap.js
@@ -2,6 +2,13 @@ export default function sitemap() {
   const baseUrl = 'https://madycloud.me';
   return [
     { url: baseUrl, lastModified: new Date() },
-    { url: `${baseUrl}/admin`, lastModified: new Date() }
+    { url: `${baseUrl}/admin`, lastModified: new Date() },
+    { url: `${baseUrl}/#home`, lastModified: new Date() },
+    { url: `${baseUrl}/#certifications`, lastModified: new Date() },
+    { url: `${baseUrl}/#education`, lastModified: new Date() },
+    { url: `${baseUrl}/#experience`, lastModified: new Date() },
+    { url: `${baseUrl}/#projects`, lastModified: new Date() },
+    { url: `${baseUrl}/#skills`, lastModified: new Date() },
+    { url: `${baseUrl}/#contact`, lastModified: new Date() }
   ];
 }
